bde3b9b82824b60ba88fa9f9139c9830c4d5957f
mnkakon <mahmudunnobi11@gmail.com>
2017-06-03 20:23:09 +0600

    all image and hover added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,52 +1,37 @@
-// external js: isotope.pkgd.js
-
 
 $(window).on("load", function() {
-    // init Isotope
-var $grid = $('.grid').isotope({
-  layoutMode: 'fitRows',
-  fitRows: {
-    gutter: -10
-  }
-});
+  var $grid = $('.grid').isotope({
+    layoutMode: 'fitRows',
+    fitRows: {
+      gutter: -10
+    }
+  });
 
-// filter functions
-var filterFns = {
-  
-};
+  // filter functions
+  var filterFns = {
+    
+  };
 
-// bind filter button click
-$('#filters').on( 'click', 'li', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
+  // bind filter button click
+  $('#filters').on( 'click', 'li', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
 
-
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'li', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
+    $(".background-list > li").click(function() {
+    console.log($(this).text());
+     $(".cat-text").text($(this).text()); 
   });
 });
-});
-
-
-  
-/*$(document).ready(function(){
-        $('.dropdown-toggle').dropdown()
-    });*/
-
 
 $(window).scroll(function(){
   $(this).scrollTop()>500?
   $(".scroll-up").fadeIn():
   $(".scroll-up").fadeOut()
 }),
+
 $('a[href="#totop"]').click(function(){
   return $("html, body").animate({scrollTop:0},"slow"),!1
 });
